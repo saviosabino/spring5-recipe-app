3a0d0b9dfa9d54a3731ae021d08393d60763fc87
saviosabino <saviopp@gmail.com>
2020-08-31 21:11:36 -0300

    Testing spring dev tools

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,13 +1,22 @@
 package guru.springframework.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
+	
+	private int counter=0;
+	
 
 	@RequestMapping({"", "/", "/index"})
-	public String getIndexPage() {
+	public String getIndexPage(Model model) {
+		
+		this.counter = counter + 1;
+		System.out.println("controller requested: index. times:" + counter);
+		model.addAttribute("counter", counter);
+		
 		return "index";
 		
 	}
