92645964d0ba8b4ca5032f8ce1ad6532faafa334
saviosabino <saviopp@gmail.com>
2020-09-03 22:34:20 -0300

    adding enum

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -18,8 +18,8 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    //todo add
-    //private Difficulty difficulty;
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
     
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "receipe")
     private Set<Ingredient> ingredients; 
@@ -116,6 +116,14 @@ public class Recipe {
 
 	public void setIngredients(Set<Ingredient> ingredients) {
 		this.ingredients = ingredients;
+	}
+
+	public Difficulty getDifficulty() {
+		return difficulty;
+	}
+
+	public void setDifficulty(Difficulty difficulty) {
+		this.difficulty = difficulty;
 	}
     
     
