93dfdd66e469eb1f758ad38798e3cc5c3f198d73
saviosabino <saviopp@gmail.com>
2020-09-09 11:50:41 -0300

    adding transactional

--- src/main/java/guru/springframework/bootstrap/RecipeBootstrap.java
+++ src/main/java/guru/springframework/bootstrap/RecipeBootstrap.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import guru.springframework.domain.*;
 import guru.springframework.repositories.*;
@@ -28,6 +29,7 @@ public class RecipeBootstrap  implements ApplicationListener<ContextRefreshedEve
     }
 
 	@Override
+	@Transactional
 	public void onApplicationEvent(ContextRefreshedEvent event) {
 		log.debug("loading springdata");
 		recipeRepository.saveAll(getRecipes());		
