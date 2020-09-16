2064b6ef393466030733f5d496d0cb38fb7fd966
saviosabino <saviopp@gmail.com>
2020-09-15 21:59:02 -0300

    @ExtendWith

--- src/test/java/guru/springframework/repositories/UnitOfMeasureRepositoryIT.java
+++ src/test/java/guru/springframework/repositories/UnitOfMeasureRepositoryIT.java
@@ -6,11 +6,14 @@ import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import guru.springframework.domain.UnitOfMeasure;
 
+@ExtendWith(SpringExtension.class)
 @DataJpaTest
 public class UnitOfMeasureRepositoryIT {
 
